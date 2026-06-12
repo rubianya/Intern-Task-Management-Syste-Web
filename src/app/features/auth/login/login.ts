f91f6defe14b915ebdaed999462c058a22f54c70
Anyarat Namwong <anyarat.na@yipintsoi.com>
2026-06-12 14:07:25 +0700

    edit

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -14,50 +14,37 @@ export class Login {
 
   email = '';
   password = '';
-  private loginUrl = 'http://localhost:8080/api/login';
 
   constructor(private router: Router, private http: HttpClient) {}
 
   login() {
 
-    if (!this.email || !this.password) {
-      alert('กรุณากรอกอีเมลและรหัสผ่านให้ครบถ้วน!');
-      return;
-    }
+    if (this.email && this.password) {
 
-    const loginPayload = {
-      email: this.email,
-      password: this.password
-    };
+      const loginPayload = {
+        email: this.email,
+        password: this.password
+      };
+      this.http.post('http://localhost:8080/api/login', loginPayload).subscribe({
+        next: (response: any) => {
 
-    this.http.post(this.loginUrl, loginPayload).subscribe({
-    next: (response: any) => {
-        console.log('Login สำเร็จ', response);
+          console.log('Login สำเร็จ', response);
 
-        if (response.active == 0) { 
-          alert('บัญชีนี้ถูกปิดใช้งานชั่วคราว ไม่สามารถเข้าสู่ระบบได้!');
-          return;
-        }
+          localStorage.setItem('full_name', response.full_name);
+          localStorage.setItem('role', response.role);
+          localStorage.setItem('token', response.token);
 
-        // localStorage.setItem('full_name', response.fullName || response.full_Name);
-        localStorage.setItem('role', response.role);
-
-        if (response.role === 'ADMIN') {
-          alert('ยินดีต้อนรับผู้ดูแลระบบ');
           this.router.navigate(['/dashboard']);
-        } else {
-          alert('เข้าสู่ระบบสำเร็จ');
-          this.router.navigate(['/dashboard']);
-        }
-      },
-      error: (error) => {
-        console.error('Login ล้มเหลว', error);
-        if (error.error && error.error.message) {
-          alert(error.error.message); 
-        } else {
+        },
+        error: (error) => {
+          console.error('Login ล้มเหลว', error);
           alert('อีเมลหรือรหัสผ่านไม่ถูกต้อง ลองใหม่อีกครั้ง!');
         }
-      }
-    });
+      });
+
+    } else {
+      alert('กรุณากรอกอีเมลและรหัสผ่านให้ครบถ้วน!');
+    }
+
   }
 }
